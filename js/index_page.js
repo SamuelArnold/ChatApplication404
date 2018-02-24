560fcabf96acf099e6c4f1b0e29f29eea8ac138d
Tyler Cypher <tcyph33@gmail.com>
2018-02-24 01:45:13 -0500

    Continued work on index.html UI, added auto scroll when sending messge, changed color of message sent to different than recieved

---
+++ js/index_page.js
@@ -0,0 +1,5 @@
+//Hide scroll bar on chat messages
+var parent = document.getElementById('chat_container');
+var child = document.getElementById('chat');
+child.style.paddingRight =
+    child.offsetWidth - child.clientWidth + 2 + "px"; //2px added to ensure fully hidden
