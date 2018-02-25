918fabd0bd03abc4795a0f28b53a08dbd9db588b
Tyler Cypher <tcyph33@gmail.com>
2018-02-24 23:05:44 -0500

    Scroll down chat div

--- js/index_page.js
+++ js/index_page.js
@@ -3,3 +3,7 @@ var parent = document.getElementById('chat_container');
 var child = document.getElementById('chat');
 child.style.paddingRight =
     child.offsetWidth - child.clientWidth + 2 + "px"; //2px added to ensure fully hidden
+
+window.onload = function(){
+  $("#chat").animate({ scrollTop: $('#chat').prop("scrollHeight")}, 1000);
+};
