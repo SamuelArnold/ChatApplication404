af6039e7898ad87670f3db7e94ae939de12be87c
Tyler Cypher <tcyph33@gmail.com>
2018-02-25 23:29:24 -0500

    added drop menu for small screen sizes

--- js/index_page.js
+++ js/index_page.js
@@ -1,3 +1,6 @@
+var mediaQuery = window.matchMedia("(min-width: 750px)");
+
+
 //Hide scroll bar on chat messages
 var parent = document.getElementById('chat_container');
 var child = document.getElementById('chat');
@@ -7,3 +10,22 @@ child.style.paddingRight =
 window.onload = function(){
   $("#chat").animate({ scrollTop: $('#chat').prop("scrollHeight")}, 1000);
 };
+
+//toggle menu for small screens and mobile devices
+$("#menu_icon").click(function(){
+    $("#drop_menu").slideToggle();
+    $(this).toggleClass("upright rotated");
+});
+
+
+//hide drop menu if it is down and window is resized
+//to where drop menu is no longer required
+function hideDropMenu(){
+    if(!mediaQuery.match){
+        $("#drop_menu").hide();
+        $("#menu_icon").removeClass("rotated");
+        $("#menu_icon").addClass("upright");
+    }
+}
+
+mediaQuery.addListener(hideDropMenu);
