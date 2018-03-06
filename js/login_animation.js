73b1eb8d52ddbfa13cd821b1973233781c4ecfec
Tyler Cypher <tcyph33@gmail.com>
2018-03-05 23:57:21 -0500

    updated fblogin page to be responsive

--- js/login_animation.js
+++ js/login_animation.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 
     $("#heading").fadeIn();
-    $("#small_logo").fadeIn();
-    setTimeout(function(){$("#p_username").fadeIn();}, 200);
-    setTimeout(function(){$("#username").fadeIn();}, 325);
-    setTimeout(function(){$("#p_password").fadeIn();}, 450);
-    setTimeout(function(){$("#password").fadeIn();}, 575);
-    setTimeout(function(){$("#submit_button").fadeIn();}, 700);
-    setTimeout(function(){$("#clear_button").fadeIn();}, 700);
+    setTimeout(function(){$("#small_logo").fadeIn();}, 200);
+    setTimeout(function(){$("#login_text").fadeIn();}, 325);
+    setTimeout(function(){$("#fb_login").fadeIn();}, 450);
 });
