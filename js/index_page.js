a3986bda7fc2af14d98e711506b9595d79025df8
Tyler Cypher <tcyph33@gmail.com>
2018-03-06 18:07:43 -0500

    Added new margins around messages

--- js/index_page.js
+++ js/index_page.js
@@ -5,7 +5,7 @@ var mediaQuery = window.matchMedia("(min-width: 750px)");
 var parent = document.getElementById('chat_container');
 var child = document.getElementById('chat');
 child.style.paddingRight =
-    child.offsetWidth - child.clientWidth + 2 + "px"; //2px added to ensure fully hidden
+    child.offsetWidth - child.clientWidth + 12 + "px"; //2px added to ensure fully hidden
 
 window.onload = function(){
    $("#chat").animate({ scrollTop: $("chat").prop("scrollHeight")}, 1000);
